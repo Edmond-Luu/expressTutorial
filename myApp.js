755ccca97b0144aadfa8201d8d1540a413dfb67d
Edmond Luu <eluu472@gmail.com>
2022-12-06 00:41:31 +0000

    6

--- myApp.js
+++ myApp.js
@@ -2,26 +2,39 @@ let express = require('express');
 let app = express();
 const mySecret = process.env['MESSAGE_STYLE']
 
+app.use(function middleware(req, res, next){
+  console.log(`${req.method} ${req.path} - ${req.ip}"`)
+  next();
+});
+
+
 // app.get("/", (req, res) => {
 //   res.send("Hello Express");
 // })
+
 
 app.use("/public", express.static(__dirname + "/public"))
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
-})
+});
 
 app.get("/json", (req, res) => {
   const resText = "Hello json"
   res.json({
     "message": process.env.MESSAGE_STYLE === "uppercase" ? resText.toUpperCase() : resText
     });
-})
+});
 
-
-
-
+app.get("/now", (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+},
+       (req, res) => {
+         res.send({
+           time: req.time
+         })
+       })
 
 console.log("Hello World")
 

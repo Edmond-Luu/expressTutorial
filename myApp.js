6f446d24683c546203ecb54aaf80fdfe1415eff7
Edmond Luu <eluu472@gmail.com>
2022-12-06 00:44:46 +0000

    7

--- myApp.js
+++ myApp.js
@@ -36,6 +36,13 @@ app.get("/now", (req, res, next) => {
          })
        })
 
+app.get("/:word/echo", (req, res) => {
+  const {word} = req.params
+  res.json({
+    echo: word
+  })
+})
+
 console.log("Hello World")
 
 
